e648e48a6a97e78d5c6a26db26b87acc14a00a71
lucasfeitosapr <lucasfeitosapr@gmail.com>
2020-03-21 00:58:32 -0400

    Main function
    Impl of main app function with alarm sound when time is up.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    var countdown = 0.0;
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    
+    let eggTime = ["Soft": 2, "Medium": 420.0, "Hard": 720.0]
+    
+    @IBAction func hardnessCheck(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        print(eggTime[hardness]!)
+        countdown = eggTime[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        if countdown > 0{
+            print("\(countdown) second(s) remaining")
+            countdown -= 1
+        } else {
+            playSound(withCurrentNote: Constants.Sounds.alarm.rawValue)
+            timer.invalidate()
+        }
+    }
+    
+    func playSound(withCurrentNote: String) {
+        let url = Bundle.main.url(forResource: withCurrentNote, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
 
-
+enum Constants {
+    enum Sounds: String {
+        case alarm = "alarm_sound";
+    }
 }

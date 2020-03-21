59bf2ec76751e4e660e5886d4fa07ddc88510ad3
lucasfeitosapr <lucasfeitosapr@gmail.com>
2020-03-21 01:32:15 -0400

    Adding ProgressBar
    Adding progressbar to eggtimer.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,26 +11,33 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    var countdown = 0.0;
+    @IBOutlet weak var progressBar: UIProgressView!
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
     var player: AVAudioPlayer!
     
-    let eggTime = ["Soft": 2, "Medium": 420.0, "Hard": 720.0]
+    let eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
     
     @IBAction func hardnessCheck(_ sender: UIButton) {
         
         timer.invalidate()
         let hardness = sender.currentTitle!
         print(eggTime[hardness]!)
-        countdown = eggTime[hardness]!
+        totalTime = eggTime[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
     }
     
     @objc func updateTimer(){
-        if countdown > 0{
-            print("\(countdown) second(s) remaining")
-            countdown -= 1
+        if secondsPassed < totalTime{
+            
+            secondsPassed += 1
+            progressBar.setProgress(Float(secondsPassed)/Float(totalTime), animated: true)
+
         } else {
             playSound(withCurrentNote: Constants.Sounds.alarm.rawValue)
             timer.invalidate()
